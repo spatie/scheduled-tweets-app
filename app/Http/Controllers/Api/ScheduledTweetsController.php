f6fabb24a6d49c87e9f7b96fff4df7d8b6caab43
freek <freek@spatie.be>
2018-07-08 23:40:15 +0200

    format

--- app/Http/Controllers/Api/ScheduledTweetsController.php
+++ app/Http/Controllers/Api/ScheduledTweetsController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Resources\ScheduledTweetResource;
 use App\Models\ScheduledTweet;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class RegisterController extends Controller
 {
